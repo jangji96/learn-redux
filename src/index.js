368b79392faa6d21c753932e32385392c52c468c
jangji96 <xxwlgns@naver.com>
2021-06-16 10:17:57 +0900

    리덕스 개발자도구 적용하기

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { createStore } from 'redux';
 import rootReducer from './modules';
 // import './exercise'
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 // console.log(store.getState());
 
 ReactDOM.render(
